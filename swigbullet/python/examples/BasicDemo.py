d4a8a04433353693c084784ee0a3e7c4ad2d3f13
ousttrue <ousttrue@gmail.com>
2012-01-28 17:48:46 +0900

    remove Demo.py

--- swigbullet/python/examples/BasicDemo.py
+++ swigbullet/python/examples/BasicDemo.py
@@ -1,161 +1,227 @@
+# coding: utf-8
 import sys
 from OpenGL.GL import *
-from OpenGL.GLU import *
+import numpy
 import swigbullet as bullet
-#from bulletdemo.opengl.sdl import main, DemoApplication
-from bulletdemo.opengl.glut import main, DemoApplication
+#from swigbullet import GL_ShapeDrawer
+from bulletdemo.camera import Camera
+from bulletdemo.bulletworld import BulletWorld
+from bulletdemo import vector3
+#from bulletdemo.profiler import Profiler
+#from bulletdemo.texture import Texture
+from swigbullet import Profiler
+from swigbullet import Texture
 
 
-# create 125 (5x5x5) dynamic object
+#create 125 (5x5x5) dynamic object
 ARRAY_SIZE_X=5
 ARRAY_SIZE_Y=5
 ARRAY_SIZE_Z=5
 
-# maximum number of objects (and allow user to shoot additional boxes)
-MAX_PROXIES=(ARRAY_SIZE_X*ARRAY_SIZE_Y*ARRAY_SIZE_Z + 1024)
-
-# scaling of the objects (0.1 = 20 centimeter boxes )
+#scaling of the objects (0.1 = 20 centimeter boxes )
 SCALING=1.0
+
 START_POS_X=-5
 START_POS_Y=-5
 START_POS_Z=-3
 
 
-class BasicDemo(DemoApplication):
-
+class Controller(object):
     def __init__(self):
-        super(BasicDemo, self).__init__()
-        self.m_broadphase=None
-        self.m_dispatcher=None
-        self.m_solver=None
-        self.m_collisionConfiguration=None
+        self.view=Camera(50)
+        # bullet
+        self.world=BulletWorld()
+        self.createGround()
+        self.createCubes()
+        self.m_clock=bullet.btClock()
+        # gldrawer
+        self.profiler=Profiler()
+        self.texture=Texture()
+        # scene
+        self.m_textureenabled=True
+        self.m_enableshadows=True
+        self.m_sundirection=vector3.mul((1,-2,1), 1000.0)
+        self.m_debugMode=0
+        #
+        self.light_ambient = numpy.array([0.2, 0.2, 0.2, 1.0], 'f')
+        self.light_diffuse = numpy.array([1.0, 1.0, 1.0, 1.0], 'f')
+        self.light_specular = numpy.array([1.0, 1.0, 1.0, 1.0], 'f')
+        # light_position is NOT default value
+        self.light_position0 = numpy.array([1.0, 10.0, 1.0, 0.0], 'f')
+        self.light_position1 = numpy.array([-1.0, -10.0, -1.0, 0.0], 'f')
 
-    def __del__(self):
-        self.exitPhysics()
+        self.w=1
+        self.h=1
+        self.is_initialized=False
 
-    def createAndAppendBody(self, shape, mass, transform):
-        # rigidbody is dynamic if and only if mass is non zero, otherwise static
-        isDynamic = (mass != 0.0);
-
-        localInertia=(0,0,0);
-        if (isDynamic):
-            shape.calculateLocalInertia(mass,localInertia);
-
-        # using motionstate is recommended, 
-        # it provides interpolation capabilities, and only synchronizes 'active' objects
-        myMotionState = bullet.btDefaultMotionState(transform);
-        rbInfo=bullet.btRigidBodyConstructionInfo(mass,
-                myMotionState,
-                shape,
-                localInertia);
-        body = bullet.btRigidBody(rbInfo);
-
-        # add the body to the dynamics world
-        self.m_dynamicsWorld.addRigidBody(body);
-        self.m_keep.append((shape, myMotionState, body));
-
-    def	initPhysics(self):
-        self.setTexturing(True);
-        #self.setShadows(True);
-        #self.setDebugMode(bullet.btIDebugDraw.DBG_NoHelpText)
-
-        self.setCameraDistance(SCALING*50.0);
-
-        # collision configuration contains default setup for memory, collision setup
-        self.m_collisionConfiguration = bullet.btDefaultCollisionConfiguration();
-
-        # use the default collision dispatcher. 
-        # For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
-        self.m_dispatcher = bullet.btCollisionDispatcher(self.m_collisionConfiguration);
-
-        self.m_broadphase = bullet.btDbvtBroadphase();
-
-        # the default constraint solver. 
-        # For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
-        self.m_solver = bullet.btSequentialImpulseConstraintSolver();
-
-        self.m_dynamicsWorld = bullet.btDiscreteDynamicsWorld(
-                self.m_dispatcher,
-                self.m_broadphase,
-                self.m_solver,
-                self.m_collisionConfiguration);
-        self.m_dynamicsWorld.setGravity((0,-10,0));
-
-        self.initGroundShape()
-        self.initRigidBodies()
-
-    def initGroundShape(self):
-        # create a few basic rigid bodies
-        groundShape = bullet.btBoxShape(
-                (50.0, 50.0, 50.0));
-        
+    def createGround(self):
+        if not self.world:
+            return
+        groundShape = bullet.btBoxShape((50.0, 50.0, 50.0));
         groundTransform=bullet.btTransform();
         groundTransform.setIdentity();
         groundTransform.setOrigin((0,-50,0));
+        self.world.localCreateRigidBody(0.0, groundTransform, groundShape)
 
-        self.createAndAppendBody(groundShape, 0.0, groundTransform)
-
-    def initRigidBodies(self):
-        # create a few dynamic rigidbodies
-        # Re-using the same collision is better for memory usage and performance
+    def createCubes(self):
+        if not self.world:
+            return
         colShape = bullet.btBoxShape((SCALING*1,SCALING*1,SCALING*1));
-
-        # Create Dynamic Objects
         startTransform=bullet.btTransform();
         startTransform.setIdentity();
 
         start_x = START_POS_X - ARRAY_SIZE_X/2;
         start_y = START_POS_Y;
         start_z = START_POS_Z - ARRAY_SIZE_Z/2;
-
         for k in range(ARRAY_SIZE_Y):
             for i in range(ARRAY_SIZE_X):
                 for j in range(ARRAY_SIZE_Z):
-                    startTransform.setOrigin((
-                        SCALING* (2.0*i + start_x),
-                        SCALING* (20+2.0*k + start_y),
-                        SCALING* (2.0*j + start_z)
-                        ));
+                    startTransform.setOrigin(vector3.mul(
+                        (
+                            (2.0*i + start_x),
+                            (20+2.0*k + start_y),
+                            (2.0*j + start_z)
+                            ), 
+                        SCALING));
+                    self.world.localCreateRigidBody(1.0, startTransform, colShape)
 
-                    self.createAndAppendBody(colShape, 1.0, startTransform)
+    def onResize(self, w, h):
+        glViewport(0, 0, w, h)
+        self.view.resize(w, h)
+        self.w=w
+        self.h=h
 
-    def	exitPhysics(self):
-        print 'exitPhysics'
-        # cleanup in the reverse order of creation/initialization
-        # remove the rigidbodies from the dynamics world and delete them
-        for i, colObj in enumerate(self.m_dynamicsWorld.getCollisionObjectArray()):
-            #print i, 'removeCollisionObject', colObj
-            self.m_dynamicsWorld.removeCollisionObject(colObj);
-        del self.m_dynamicsWorld;
-        del self.m_solver;
-        del self.m_dispatcher;
-        del self.m_collisionConfiguration;
-        del self.m_keep
+    def onLeftDown(self, x, y):
+        print 'onLeftDown', x, y
 
-    def clientMoveAndDisplay(self):
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
-        # simple dynamics world doesn't handle fixed-time-stepping
-        ms = self.getDeltaTimeMicroseconds();
-        # step the simulation
-        if (self.m_dynamicsWorld):
-            self.m_dynamicsWorld.stepSimulation(ms / 1000000.0);
-            # optional but useful: debug drawing
-            self.m_dynamicsWorld.debugDrawWorld();
-        self.renderme(); 
-        glFlush();
-        self.swapBuffers();
+    def onLeftUp(self, x, y):
+        print 'onLeftUp', x, y
 
-    def displayCallback(self):
-        pass
+    def onMiddleDown(self, x, y):
+        print 'onMiddleDown', x, y
 
-    def	clientResetScene(self):
-        self.exitPhysics()
-        self.initPhysics()
+    def onMiddleUp(self, x, y):
+        print 'onMiddleUp', x, y
+
+    def onRightDown(self, x, y):
+        print 'onRightDown', x, y
+
+    def onRightUp(self, x, y):
+        print 'onRightUp', x, y
+
+    def onMotion(self, x, y):
+        print 'onMotion', x, y
+
+    def onWheel(self, d):
+        print 'onWheel', d
+
+    def onKeyDown(self, key):
+        print 'onKeyDown', key
+        if key=='=':
+            self.serialize();
+        elif key=='+':
+            self.m_ShootBoxInitialSpeed+=10.0
+        elif key=='-':
+            self.m_ShootBoxInitialSpeed-=10.0
+        elif key==ord('q'):
+            sys.exit(0)
+        else:
+            print 'unknown key:', key
+
+    def onUpdate(self, d):
+        if not self.world:
+            return
+        self.world.update(self.getDeltaTimeMicroseconds())
+
+    def getDeltaTimeMicroseconds(self):
+        dt = self.m_clock.getTimeMicroseconds();
+        self.m_clock.reset();
+        return dt;
+
+    def onInitialize(self):
+        glLightfv(GL_LIGHT0, GL_AMBIENT, self.light_ambient);
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, self.light_diffuse);
+        glLightfv(GL_LIGHT0, GL_SPECULAR, self.light_specular);
+        glLightfv(GL_LIGHT0, GL_POSITION, self.light_position0);
+
+        glLightfv(GL_LIGHT1, GL_AMBIENT, self.light_ambient);
+        glLightfv(GL_LIGHT1, GL_DIFFUSE, self.light_diffuse);
+        glLightfv(GL_LIGHT1, GL_SPECULAR, self.light_specular);
+        glLightfv(GL_LIGHT1, GL_POSITION, self.light_position1);
+
+        glEnable(GL_LIGHTING);
+        glEnable(GL_LIGHT0);
+        glEnable(GL_LIGHT1);
+
+        glShadeModel(GL_SMOOTH);
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LESS);
+        glClearColor(0.7, 0.7 ,0.7, 0.0);
+        #glClearColor(0.9, 0.5, 0.0, 0.0)
+        self.is_initialized=True
+
+    def draw(self):
+        if not self.is_initialized:
+            self.onInitialize()
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        # render
+        self.view.draw()
+        if self.world:
+            if self.m_enableshadows:
+                bullet.renderwithshadow(self.world.m_dynamicsWorld, 
+                        self.m_textureenabled, self.texture,
+                        self.m_sundirection, self.m_debugMode)
+            else:
+                bullet.render(self.world.m_dynamicsWorld, self.m_debugMode)
+            # profiler
+            self.view.setOrthographicProjection();
+            self.profiler.render(self.world.m_idle, self.m_debugMode,
+                    self.w, self.h)
+            self.view.resetPerspectiveProjection();
+
+        glFlush()
 
 
 if __name__=="__main__":
-    main(sys.argv, 
-            1024, 600, 
-            "Bullet Physics Demo. http://bulletphysics.org", 
-            BasicDemo())
+    controller=Controller()
+    #import glglue.glut
+    #glglue.glut.mainloop(controller, width=1024, height=600, 
+    #        title="Bullet Physics Demo. http:#bulletphysics.org")
+
+    #import glglue.wgl
+    #glglue.wgl.mainloop(controller, width=640, height=480, title="sample")
+
+    import pygame
+    from pygame.locals import *
+    pygame.init()
+    size=(1024, 600)
+    pygame.display.gl_set_attribute(pygame.GL_STENCIL_SIZE, 2)
+    screen = pygame.display.set_mode(size,
+            HWSURFACE | OPENGL | DOUBLEBUF)
+
+    controller.onResize(*size)
+
+    clock = pygame.time.Clock()
+    is_running=True
+    while is_running:
+        #pressed = pygame.key.get_pressed()
+
+        # event handling
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                is_running=False
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    is_running=False
+                else:
+                    controller.onKeyDown(event.key)
+            
+        # update
+        d = clock.tick()
+        if d>0:
+            controller.onUpdate(d)
+            controller.draw()
+            pygame.display.flip()
+
+    sys.exit(0)
 
--- swigbullet/python/examples/bulletdemo/bulletworld.py
+++ swigbullet/python/examples/bulletdemo/bulletworld.py
@@ -5,38 +5,10 @@ from bulletdemo import vector3
 class BulletWorld(object):
 
     def __init__(self):
-        self.m_shootBoxShape=None
-        self.m_ShootBoxInitialSpeed=40.0
         self.m_defaultContactProcessingThreshold=bullet.BT_LARGE_FLOAT
         self.m_idle=False
-        self.use6Dof=False
-        self.mousePickClamping=30.0
-        self.gPickingConstraintId=0
-        self.gHitPos=(-1,-1,-1)
-        self.gOldPickingDist=0.0
-        self.m_clock=bullet.btClock()
         self.keep=[]
         # setup
-        self.createWorld();
-
-    def __del__(self):
-        self.clear();
-        if self.m_shootBoxShape:
-            del self.m_shootBoxShape
-        del self.m_dynamicsWorld
-        del self.m_solver
-        del self.m_broadphase
-        del self.m_dispatcher
-        del self.m_collisionConfiguration
-        print self
-
-    def	clear(self):
-        #cleanup in the reverse order of creation/initialization
-        #remove the rigidbodies from the dynamics world and delete them
-        self.m_dynamicsWorld.getCollisionObjectArray().clear()
-        self.keep=[]
-
-    def createWorld(self):
         #collision configuration contains default setup for memory, collision setup
         self.m_collisionConfiguration = bullet.btDefaultCollisionConfiguration();
 
@@ -59,13 +31,27 @@ class BulletWorld(object):
 
         self.m_dynamicsWorld.setGravity((0.0, -10.0, 0.0));
 
+    def __del__(self):
+        self.clear();
+        del self.m_dynamicsWorld
+        del self.m_solver
+        del self.m_broadphase
+        del self.m_dispatcher
+        del self.m_collisionConfiguration
+
+    def	clear(self):
+        #cleanup in the reverse order of creation/initialization
+        #remove the rigidbodies from the dynamics world and delete them
+        self.m_dynamicsWorld.getCollisionObjectArray().clear()
+        self.keep=[]
+
     def localCreateRigidBody(self, mass, startTransform, shape):
         assert((shape==None or shape.getShapeType() != bullet.INVALID_SHAPE_PROXYTYPE));
 
         #rigidbody is dynamic if and only if mass is non zero, otherwise static
         localInertia=(0,0,0);
         if (mass != 0.0):
-            shape.calculateLocalInertia(mass, localInertia);
+            localInertia=shape.calculateLocalInertia(mass, localInertia);
 
         #using motionstate is recommended, it provides interpolation capabilities, 
         # and only synchronizes 'active' objects
@@ -73,7 +59,7 @@ class BulletWorld(object):
         myMotionState = bullet.btDefaultMotionState(startTransform);
         body = bullet.btRigidBody(
                 bullet.btRigidBodyConstructionInfo(mass,myMotionState,shape,localInertia));
-        #body.setContactProcessingThreshold(self.m_defaultContactProcessingThreshold);
+        body.setContactProcessingThreshold(self.m_defaultContactProcessingThreshold);
 
         self.keep.append((body, myMotionState, shape))
 
@@ -81,10 +67,17 @@ class BulletWorld(object):
 
         return body;
 
-    def getDeltaTimeMicroseconds(self):
-        dt = self.m_clock.getTimeMicroseconds();
-        self.m_clock.reset();
-        return dt;
+    def removeLastObject(self):
+        numObj = self.m_dynamicsWorld.getNumCollisionObjects();
+        if numObj==0:
+            return
+        obj = m_dynamicsWorld.getCollisionObjectArray()[numObj-1];
+        m_dynamicsWorld.removeCollisionObject(obj);
+
+    def update(self, ms):
+        if self.m_idle:
+            return
+        self.m_dynamicsWorld.stepSimulation(ms / 1000000.0);
 
     def serialize(self):
         maxSerializeBufferSize = 1024*1024*5;
@@ -92,192 +85,4 @@ class BulletWorld(object):
         self.m_dynamicsWorld.serialize(serializer);
         with open("testFile.bullet","wb") as f2:
             f2.write(serializer.getBufferPointer(), serializer.getCurrentBufferSize());
-
-    def setShootBoxShape(self):
-        if (not self.m_shootBoxShape):
-            box = bullet.btBoxShape((0.5, 0.5, 0.5));
-            box.initializePolyhedralFeatures();
-            self.m_shootBoxShape = box;
-
-    def shootBox(self, camera, x, y):
-        if not self.m_dynamicsWorld:
-            return
-
-        self.setShootBoxShape();
-
-        camPos=camera.getCameraPosition();
-        destination=camera.getRayTo(x,y);
-
-        startTransform=bullet.btTransform();
-        startTransform.setIdentity();
-        startTransform.setOrigin(camPos);
-
-        body = self.localCreateRigidBody(1.0, startTransform, self.m_shootBoxShape);
-        body.setLinearFactor((1,1,1));
-
-        linVel=[d-c for d, c in zip(destination, camPos)]
-        linVel=vector3.normalize(linVel);
-        linVel=vector3.mul(linVel, self.m_ShootBoxInitialSpeed)
-
-        body.getWorldTransform().setOrigin(camPos);
-        body.getWorldTransform().setRotation(bullet.btQuaternion(0,0,0,1));
-        body.setLinearVelocity(linVel);
-        body.setAngularVelocity((0,0,0));
-        body.setCcdMotionThreshold(0.5);
-        body.setCcdSweptSphereRadius(0.9);
-
-    def update(self, ms=None):
-        if (not self.m_idle):
-            # simple dynamics world doesn't handle fixed-time-stepping
-            if not ms:
-                ms = self.getDeltaTimeMicroseconds();
-
-            # step the simulation
-            if (self.m_dynamicsWorld):
-                self.m_dynamicsWorld.stepSimulation(ms / 1000000.0);
-                # optional but useful: debug drawing
-                #self.m_dynamicsWorld.debugDrawWorld();
-
-    def debugDraw(self):
-        if (self.m_dynamicsWorld):
-            self.m_dynamicsWorld.debugDrawWorld();
-
-    def removeLastObject(self):
-        numObj = self.m_dynamicsWorld.getNumCollisionObjects();
-        if (numObj>0):
-            obj = m_dynamicsWorld.getCollisionObjectArray()[numObj-1];
-            m_dynamicsWorld.removeCollisionObject(obj);
-            # todo
-
-    def keyboardCallback(self, key, x, y):
-        if key=='=':
-            self.serialize();
-        elif key=='+':
-            self.m_ShootBoxInitialSpeed+=10.0
-        elif key=='-':
-            self.m_ShootBoxInitialSpeed-=10.0
-        else:
-            print 'unknown key:', key
-
-    def pickStart(self, camera, x, y):
-        # add a point to point constraint for picking
-        if (self.m_dynamicsWorld):
-            rayTo=camera.getRayTo(x,y);
-            rayFrom=None
-            if (camera.m_ortho):
-                rayFrom=(rayTo[0], To[1], -100.0);
-            else:
-                rayFrom = camera.getCameraPosition();
-            rayCallback=bullet.ClosestRayResultCallback(rayFrom, rayTo);
-            self.m_dynamicsWorld.rayTest(rayFrom, rayTo, rayCallback);
-            if (rayCallback.hasHit()):
-                body = bullet.btRigidBody.upcast(rayCallback.m_collisionObject);
-                if (body):
-                    print 'hit'
-                    # other exclusions?
-                    if (not (body.isStaticObject() or body.isKinematicObject())):
-                        print body
-                        self.pickedBody = body;
-                        self.pickedBody.setActivationState(bullet.DISABLE_DEACTIVATION);
-                        #pickPos = rayCallback.m_hitPointWorld;
-                        pickPos = rayCallback.m_hitPointWorld
-                        # printf("pickPos=%f,%f,%f\n",pickPos.getX(),pickPos.getY(),pickPos.getZ());
-                        localPivot = body.getCenterOfMassTransform().inverse().apply(pickPos);
-
-                        self.m_keep.append(self.m_pickConstraint)
-                        if (self.m_use6Dof):
-                            print 'm_use6Dof'
-                            tr=bullet.btTransform();
-                            tr.setIdentity();
-                            tr.setOrigin(localPivot);
-                            dof6 = bullet.btGeneric6DofConstraint(body, tr, False);
-                            dof6.setLinearLowerLimit((0,0,0));
-                            dof6.setLinearUpperLimit((0,0,0));
-                            dof6.setAngularLowerLimit((0,0,0));
-                            dof6.setAngularUpperLimit((0,0,0));
-
-                            self.m_dynamicsWorld.addConstraint(dof6);
-                            self.m_pickConstraint = dof6;
-
-                            dof6.setParam(bullet.BT_CONSTRAINT_STOP_CFM,0.8,0);
-                            dof6.setParam(bullet.BT_CONSTRAINT_STOP_CFM,0.8,1);
-                            dof6.setParam(bullet.BT_CONSTRAINT_STOP_CFM,0.8,2);
-                            dof6.setParam(bullet.BT_CONSTRAINT_STOP_CFM,0.8,3);
-                            dof6.setParam(bullet.BT_CONSTRAINT_STOP_CFM,0.8,4);
-                            dof6.setParam(bullet.BT_CONSTRAINT_STOP_CFM,0.8,5);
-
-                            dof6.setParam(bullet.BT_CONSTRAINT_STOP_ERP,0.1,0);
-                            dof6.setParam(bullet.BT_CONSTRAINT_STOP_ERP,0.1,1);
-                            dof6.setParam(bullet.BT_CONSTRAINT_STOP_ERP,0.1,2);
-                            dof6.setParam(bullet.BT_CONSTRAINT_STOP_ERP,0.1,3);
-                            dof6.setParam(bullet.BT_CONSTRAINT_STOP_ERP,0.1,4);
-                            dof6.setParam(bullet.BT_CONSTRAINT_STOP_ERP,0.1,5);
-                        else:
-                            print 'not m_use6Dof'
-                            p2p = bullet.btPoint2PointConstraint(body,localPivot);
-                            self.m_dynamicsWorld.addConstraint(p2p);
-                            self.m_pickConstraint = p2p;
-                            p2p.m_setting.m_impulseClamp = 30.0;
-                            # very weak constraint for picking
-                            p2p.m_setting.m_tau = 0.001;
-
-                        self.m_use6Dof = not self.m_use6Dof;
-
-                        # save mouse position for dragging
-                        self.gOldPickingPos = rayTo;
-                        self.gHitPos = pickPos;
-
-                        self.m_oldPickingDist  = vector3.length(
-                                vector3.sub(pickPos, rayFrom));
-                        print 'end'
-
-
-    def removePickingConstraint(self):
-        if (self.m_pickConstraint and self.m_dynamicsWorld):
-            self.m_dynamicsWorld.removeConstraint(self.m_pickConstraint);
-            self.m_pickConstraint=None
-            self.pickedBody.forceActivationState(bullet.ACTIVE_TAG);
-            self.pickedBody.setDeactivationTime(0.0);
-            self.pickedBody = 0;
-
-    def pick(self, camera, x, y):
-        if (self.m_pickConstraint):
-            #move the constraint pivot
-            if (self.m_pickConstraint.getConstraintType() == self.D6_CONSTRAINT_TYPE):
-                pickCon = bullet.btGeneric6DofConstraint.downcast(self.m_pickConstraint);
-                if (pickCon):
-                    #keep it at the same picking distance
-                    newRayTo = camera.getRayTo(x,y);
-                    oldPivotInB = pickCon.getFrameOffsetA().getOrigin();
-                    rayFrom=None;
-                    newPivotB=None;
-                    if (camera.m_ortho):
-                        newPivotB = (newRayTo[0], newRayTo[1], oldPivotInB[2]);
-                    else:
-                        rayFrom = camera.getCameraPosition();
-                        dir = vector3.mul(
-                                vector3.normalize(vector3.sub(newRayTo, rayFrom)),
-                                self.gOldPickingDist);
-
-                        newPivotB = vector3.add(rayFrom, dir);
-                    pickCon.getFrameOffsetA().setOrigin(newPivotB);
-            else:
-                pickCon = btPoint2PointConstraint.downcast(self.m_pickConstraint);
-                if (pickCon):
-                    #keep it at the same picking distance
-                    newRayTo = camera.getRayTo(x,y);
-                    oldPivotInB = pickCon.getPivotInB();
-                    rayFrom=None;
-                    newPivotB=None;
-                    if (camera.m_ortho):
-                        newPivotB = (newRayTo[0], newRayTo[1], oldPivotInB[2]);
-                    else:
-                        rayFrom = camera.getCameraPosition();
-                        dir = vector3.mul(
-                                vector3.normalize(vector3.sub(newRayTo, rayFrom)),
-                                self.gOldPickingDist);
-
-                        newPivotB = vector3.add(rayFrom, dir);
-                    pickCon.setPivotB(newPivotB);
-
 
